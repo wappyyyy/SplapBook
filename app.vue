af8905d9c897267230ef67396626edfd770b3a97
akihiko <akihiko.wakuda0529@gmail.com>
2024-04-18 09:22:00 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="m-5">
+    <!-- <NuxtWelcome /> -->
+    <button class="bg-cyan-400 text-white hover:pointer p-5 block">
+      This is tailwind button
+    </button>
+  </div>
+</template>
